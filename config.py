dc5bcd9305b7b265480649a6be0635e5cf04e547
Chowdhury-Siam <149977276+Chowdhury-Siam@users.noreply.github.com>
2024-02-17 14:55:46 +0600

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SiamRin33/Music-bot",
+    "https://github.com/Chowdhury-Siam/Music-bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
